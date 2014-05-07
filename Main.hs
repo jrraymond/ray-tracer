c350ab5f31f1d0bda3efd46e28ced89f512b0ada
Justin Raymond <justin.raymond.14@gmail.com>
2014-05-07 15:33:34 -0400

    fixed funky ppm color by clamping floats into range [0,1]

--- Main.hs
+++ Main.hs
@@ -3,7 +3,6 @@
 -
 -  Ray Tracer
 -}
-{-# LANGUAGE ForeignFunctionInterface #-}
 import Foreign
 import qualified Graphics.UI.GLUT as GLUT
 import qualified Graphics.Rendering.OpenGL as GL
@@ -20,14 +19,14 @@ main = do
     (_progName, _args) <- GLUT.getArgsAndInitialize
     ----open the main window
     _window <- GLUT.createWindow "Ray Tracer"
-    GLUT.windowSize GLUT.$= (GL.Size 800 600)
+    GLUT.windowSize GLUT.$= GL.Size 800 600
     ----set the display callback for the main window
     GLUT.displayCallback GLUT.$= display
     GLUT.reshapeCallback GLUT.$= Just reshape
     ----let GLUT take over
     GLUT.mainLoop
 reshape :: GLUT.ReshapeCallback
-reshape size = do GLUT.viewport GLUT.$= (GLUT.Position 0 0, size)
+reshape size = GLUT.viewport GLUT.$= (GLUT.Position 0 0, size)
 
 display :: GLUT.DisplayCallback
 display = do
@@ -38,6 +37,7 @@ display = do
         ht = 600
         pixels = render wd ht 
     writePPM "output.ppm" wd ht $ invertY wd ht pixels
+    print pixels
     arr <- newArray (flatten pixels) :: IO (Ptr Float)
     --arr <- FMU.new (VS.replicate 100 (1 :: Float))
     GL.drawPixels (GL.Size 800 600) (PixelData GL.RGB GL.Float arr)
@@ -45,18 +45,27 @@ display = do
     --pushes our OpenGL commands down to the systems graphics for display
     GLUT.flush
 writePPM :: String -> Int -> Int -> [Color] -> IO ()
-writePPM name w h pixels = do writeFile name string  where 
+writePPM name w h pixels = writeFile name string  where 
   toStr :: Float -> String
-  toStr = (++ " ") . show . truncate . (255*)
+  toStr = (++ " ") . show . (truncate :: Float -> Int) . (255*) . clamp
   f :: [Color] -> String
   f [] = ""
-  f ((Color r g b):ps) = toStr r ++ toStr g ++ toStr b ++ f ps
+  f (Color r g b:ps) = toStr r ++ toStr g ++ toStr b ++ f ps
   string = "P3\n" ++ show w ++ " " ++ show h ++ " 255\n" ++ f pixels
 
+{- Flips the array of pixels to account for GLUT and ppm having different
+- origins. GLUT's origin is the lower left hand corner, but ppm expects 
+- the origin to be in the upper right hand corner.
+-}
 invertY :: Int -> Int -> [Color] -> [Color]
-invertY wd ht cs = map (\(_,v) -> v) $ sortBy g $ map f $ zip [0..] cs where
+invertY wd ht cs = map snd $ sortBy g $ zipWith (curry f) [0..] cs where
   f :: (Int,Color) -> (Int,Color)
   f (i,v) = (wd * (ht - i `div` wd) + i `mod` wd,v)
   g :: (Int,Color) -> (Int,Color) -> Ordering
   g (i,_) (j,_) = i `compare` j
 
+{- Some of our pixel floats are outside the range [0,1], GLUT clamps 
+- all float in the range [0,1], but writing a ppm does not, so we need
+- this to write the ppm -}
+clamp :: Float -> Float
+clamp x | x < 0 = 0 | x > 1 = 1 | otherwise = x
