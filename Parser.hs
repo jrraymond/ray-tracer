aa11cba14e9680f4c82d218a0fdcfd377dc472cb
Justin Raymond <justin.raymond.14@gmail.com>
2014-05-16 00:21:32 -0400

    parser 2.0 complete

--- Parser.hs
+++ Parser.hs
@@ -7,13 +7,26 @@ module Parser where
   import Debug.Trace
   import Control.Applicative hiding (many, (<|>))
   import System.Exit
+  import Control.Monad.Identity
   import Text.Parsec hiding (runParser)
   import Text.Parsec.Expr
   import Text.ParserCombinators.Parsec hiding (try)
   import Data.Map (Map)
   import qualified Data.Map as Map
   import Surfaces
+    
 
+  {- A datatype corresponding to our shape datatype with Expressions
+  - instead of floats. -}
+  data ShapeExpr = SphereE (Expr,Expr,Expr) Expr Material 
+                 | PlaneE (Expr,Expr,Expr) (Expr,Expr,Expr) (Expr,Expr,Expr) Material
+                 | TriangleE (Expr,Expr,Expr) (Expr,Expr,Expr) (Expr,Expr,Expr) Material
+                 deriving Show
+
+  {- A convenient function. Attempts to parse with p. If p fails backtracks
+  - and attempts to parse with q -}
+  (<||>) :: ParsecT s u m a -> ParsecT s u m a -> ParsecT s u m a
+  p <||> q = (try p) <|> q
   eol :: Parser ()
   eol = do oneOf "\n\r"
            return ()
@@ -23,23 +36,33 @@ module Parser where
   comment = do char '#'
                skipMany (noneOf "\r\n") <?> "comment"
 
-  floats = many float
-  --ident :: Parser String
+  ident :: Stream s m Char => ParsecT s u m [Char]
   ident = do skipMany space
              c <- letter <|> char '_'
              cs <- many (letter <|> digit <|> char '_')
              skipMany space
              return (c:cs)
              <?> "identifier"
+
+  number :: Stream s m Char => ParsecT s u m [Char]
   number = many1 digit
+
+  plus :: Stream s m Char => ParsecT s u m [Char]
   plus = char '+' *> number
+
+  minus :: Stream s m Char => ParsecT s u m [Char]
   minus = (:) <$> char '-' <*> number
+
+  integer :: Stream s m Char => ParsecT s u m [Char]
   integer = plus <|> minus <|> number
+
+  float :: Stream s m Char => ParsecT s u m Float
   float = fmap rd $ (++) <$> integer <*> dec where
     rd = read :: String -> Float
     dec = option "" $ (:) <$> char '.' <*> number
 
 
+  identColor :: Stream s m Char => ParsecT s (Map [Char] a) m a
   identColor = do skipMany space
                   c <- letter <|> char '_'
                   cs <- many (letter <|> digit <|> char '_')
@@ -50,10 +73,10 @@ module Parser where
                              Just c' -> c'
                   <?> "identifier"
 
-  --color :: Parser (String,Color) 
+  color :: Stream s m Char => ParsecT s u m ([Char], Color)
   color = do string "Color"
              key <- ident
-             char '='
+             _ <- char '='
              skipMany space 
              x <- float
              skipMany space 
@@ -62,6 +85,8 @@ module Parser where
              z <- float
              skipMany space 
              return (key,Color x y z)
+
+  color' :: Stream s m Char => ParsecT s u m Color
   color' = do string "Color"
               skipMany space 
               x <- float
@@ -72,6 +97,7 @@ module Parser where
               skipMany space 
               return (Color x y z)
 
+  readColors :: Stream s Identity Char => t -> s -> Either ParseError (Map [Char] Color)
   readColors _ s = case parse (many color) "" s of 
                          Left err -> Left err
                          Right xs -> Right (Map.fromList xs)
@@ -79,7 +105,7 @@ module Parser where
   material :: GenParser Char (Map String Color) (String,Material)
   material = do string "Material"
                 key <- ident
-                char '='
+                _ <- char '='
                 amb <- color' <|> identColor
                 dif <- color' <|> identColor
                 spe <- color' <|> identColor
@@ -96,7 +122,7 @@ module Parser where
                         Left err -> Left err
                         Right xs -> Right (Map.fromList xs)
 
-  point :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m (Float, Float, Float)
+  point :: Stream s m Char => ParsecT s u m (Float, Float, Float)
   point = do skipMany space
              _ <- char '('
              skipMany space
@@ -109,6 +135,7 @@ module Parser where
              _ <- char ')'
              skipMany space
              return (x, y, z)
+
   --TODO generalize this replicateM or something
   sepBy3 :: (Stream s m t) => ParsecT s u m a -> ParsecT s u m sep -> ParsecT s u m [a]
   sepBy3 p sep = do x <- p
@@ -118,21 +145,21 @@ module Parser where
                     z <- p
                     return (x:y:z:[])
 
-  pointExpr :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m (Expr, Expr, Expr)
+  pointExpr :: Stream s m Char => ParsecT s u m (Expr, Expr, Expr)
   pointExpr = do skipMany space
                  _ <- char '('
                  skipMany space
-                 x <- expr
+                 x <- expression
                  skipMany space
-                 y <- expr
+                 y <- expression
                  skipMany space
-                 z <- expr
+                 z <- expression
                  skipMany space
                  _ <- char ')'
                  skipMany space
                  return (x, y, z)
 
-  identMaterial :: forall s (m :: * -> *) a. Stream s m Char => ParsecT s (Map [Char] a) m a
+  identMaterial :: Stream s m Char => ParsecT s (Map [Char] a) m a
   identMaterial = do c <- letter <|> char '_'
                      cs <- many (letter <|> digit <|> char '_')
                      skipMany space
@@ -142,27 +169,27 @@ module Parser where
                                 Just c' -> c'
                      <?> "identifier"
 
-  sphere :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
+  sphere :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
   sphere = do string "Sphere"
               key <- ident
-              char '='
+              _ <- char '='
               center <- point
               radius <- float
               skipMany space
               mat <- identMaterial
               return (key, Sphere center radius mat)
 
-  sphereExpr :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
+  sphereExpr :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
   sphereExpr = do string "Sphere"
                   key <- ident
-                  char '='
+                  _ <- char '='
                   center <- pointExpr
-                  radius <- expr
+                  radius <- expression
                   skipMany space
                   mat <- identMaterial
                   return (key, SphereE center radius mat)
 
-  triangle :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
+  triangle :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
   triangle = do string "Triangle"
                 key <- ident
                 _ <- char '='
@@ -172,7 +199,7 @@ module Parser where
                 mat <- identMaterial
                 return (key, Triangle a b c mat)
 
-  triangleExpr :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
+  triangleExpr :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
   triangleExpr = do string "Triangle"
                     key <- ident
                     _ <- char '='
@@ -182,7 +209,7 @@ module Parser where
                     mat <- identMaterial
                     return (key, TriangleE a b c mat)
 
-  plane :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
+  plane :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
   plane = do string "Plane"
              key <- ident
              _ <- char '='
@@ -192,7 +219,7 @@ module Parser where
              mat <- identMaterial
              return (key, Plane a b c mat)
 
-  planeExpr :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
+  planeExpr :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
   planeExpr = do string "Plane"
                  key <- ident
                  _ <- char '='
@@ -202,151 +229,11 @@ module Parser where
                  mat <- identMaterial
                  return (key, PlaneE a b c mat)
 
-  shape :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
+  shape :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], Shape)
   shape = sphere <|> triangle <|> plane
 
-  shapeExpr :: forall s (m :: * -> *). Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
+  shapeExpr :: Stream s m Char => ParsecT s (Map [Char] Material) m ([Char], ShapeExpr)
   shapeExpr = sphereExpr <|> triangleExpr <|> planeExpr
-
-  data ShapeExpr = SphereE (Expr,Expr,Expr) Expr Material 
-                 | PlaneE (Expr,Expr,Expr) (Expr,Expr,Expr) (Expr,Expr,Expr) Material
-                 | TriangleE (Expr,Expr,Expr) (Expr,Expr,Expr) (Expr,Expr,Expr) Material
-                 deriving Show
-
-  data OpU = NegOpU | PlusOpU | SinOpU | CosOpU deriving Show
-  data OpB = PlusOpB | ProdOpB | MinusOpB | DivOpB  | PowOpB deriving Show
-  evalOpU :: OpU -> (Float -> Float)
-  evalOpU NegOpU = negate
-  evalOpU PlusOpU = id
-  evalOpU SinOpU = sin
-  evalOpU CosOpU = cos
-  evalOpB :: OpB -> (Float -> Float -> Float)
-  evalOpB PlusOpB = (+)
-  evalOpB MinusOpB = (-)
-  evalOpB ProdOpB = (*)
-  evalOpB DivOpB = (/)
-  evalOpB PowOpB = (**)
-  data Expr = UnaryNode OpU Expr
-           | BinaryNode OpB Expr Expr
-           | NumNode Float
-           | VarNodeT
-           | VarNodeX
-           | VarNodeY
-           | VarNodeZ
-    deriving Show
-  evalExpr :: Float -> Expr -> Float
-  evalExpr t (UnaryNode op e) = evalOpU op $ evalExpr t e
-  evalExpr t (BinaryNode op e1 e2) = (evalOpB op) (evalExpr t e1) (evalExpr t e2)
-  evalExpr _ (NumNode f) = f
-  evalExpr t VarNodeT = t
-  evalExpr _ _ = error "unimplemented"
-
-  evalExprTuple :: Float -> (Expr,Expr,Expr) -> (Float,Float,Float)
-  evalExprTuple t (x,y,z) = (evalExpr t x, evalExpr t y, evalExpr t z)
-
-  evalShapeExpr :: Float -> ShapeExpr -> Shape
-  evalShapeExpr t (SphereE c r mat) = Sphere (evalExprTuple t c) (evalExpr t r) mat
-  evalShapeExpr t (TriangleE a b c mat) = Triangle (evalExprTuple t a) (evalExprTuple t b) (evalExprTuple t c) mat
-  evalShapeExpr t (PlaneE a b c mat) = Plane (evalExprTuple t a) (evalExprTuple t b) (evalExprTuple t c) mat
-  evalShapeExpr _ _ = error "unimplemented"
-
-  table :: forall s u (m :: * -> *). Stream s m Char => [[Operator s u m Expr]]
-  table = [ [ Prefix (char '+' >> return (UnaryNode PlusOpU))
-            , Prefix (char '-' >> return (UnaryNode NegOpU))
-            , Prefix (string "sin" >> return (UnaryNode SinOpU))
-            , Prefix (string "cos" >> return (UnaryNode CosOpU))
-            ]
-          , [ Infix (char '*' >> return (BinaryNode ProdOpB)) AssocLeft
-            , Infix (char '/' >> return (BinaryNode DivOpB)) AssocLeft 
-            ]
-          , [ Infix (char '+' >> return (BinaryNode PlusOpB)) AssocLeft
-            , Infix (char '-' >> return (BinaryNode MinusOpB)) AssocLeft
-            ]
-          ]
-
-  term :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m Expr
-  term = fmap NumNode float <|>
-         (char 't' >> return VarNodeT) <|>
-         (char 'x' >> return VarNodeX) <|>
-         (char 'y' >> return VarNodeY) <|>
-         (char 'z' >> return VarNodeZ) 
-         <?> "term"
-
-  expr :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m Expr
-  expr = buildExpressionParser table term
-
-  {- Parser 2.0 
-  - Formal description of expression language:
-  - Expression := Expression [+-] Term |  Term
-  - Term := Term [*/] Factor | Factor
-  - Factor := (Expression) | Float
-  -}
-  {- Arithmetic operations -}
-  data Operator' = PlusOp' | MinusOp' | MultOp' | DivOp' | NegOp' | SinOp' | CosOp' deriving (Show, Eq)
-
-  {- Parse Tree for the expression lanuage 2.0 -}
-  data ParseTree = UnaryNode' Operator' ParseTree
-                 | BinaryNode' Operator' ParseTree ParseTree
-                 | NumNode' Float
-                 | VarNodeT'
-                 | Lparens
-                 | Rparens
-                 deriving Show
-  {- The following parsers parse an expression into a list of tokens -}
-  data Token = TokOp Operator' | TokLParen | TokRParen | TokNum Float | TokVarT | TokEnd deriving (Show, Eq)
-  token' :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m Token
-  token' = (char '(' >> return TokLParen) <|>
-           (char ')' >> return TokRParen) <|>
-           try (float >>= \f -> return $ TokNum f) <|>
-           (char 't' >> return TokVarT) <|>
-           (char '*' >> return (TokOp MultOp')) <|>
-           (char '/' >> return (TokOp DivOp')) <|>
-           (char '+' >> return (TokOp PlusOp')) <|>
-           (char '-' >> return (TokOp MinusOp')) <|>
-           (string "sin" >> return (TokOp SinOp')) <|>
-           (string "cos" >> return (TokOp CosOp')) <?> "operator"
-  tokenize' :: forall s u (m :: * -> *). Stream s m Char => ParsecT s u m [Token]
-  tokenize' = many token'
-  {- Now we parse the tokens into an AST -}
-  expression :: [Token] -> (ParseTree, [Token])
-  expression ts = let (termTree, ts') = term' ts in
-    case head ts' of
-       (TokOp op) | op == PlusOp' || op == MinusOp' -> 
-          let (exTree, ts'') = expression (tail ts') 
-          in (BinaryNode' op termTree exTree, ts'')
-       _ -> (termTree, ts')
-
-  term' :: [Token] -> (ParseTree, [Token])
-  term' ts = let (facTree, ts') = factor ts in
-    case head ts' of
-       (TokOp op) | op == MultOp' || op == DivOp' ->
-          let (termTree, ts'') = term' (tail ts') 
-          in (BinaryNode' op facTree termTree, ts'')
-       _ -> (facTree, ts')
-
-  factor :: [Token] -> (ParseTree, [Token])
-  factor ts = 
-     case head ts of
-        (TokNum x) -> (NumNode' x, tail ts)
-        (TokVarT) -> (VarNodeT', tail ts)
-        (TokOp op) | elem op [PlusOp', MinusOp'] -> 
-              let (facTree, ts') = factor (tail ts) 
-              in (UnaryNode' op facTree, ts')
-        TokLParen      -> 
-           let (expTree, ts') = expression (tail ts)
-           in
-              if head ts' /= TokRParen 
-              then error "Expecting right parens"
-              else (expTree, tail ts')
-        _ -> error $ "Parse error on token: " ++ show ts
-
-  parse' :: [Token] -> ParseTree
-  parse' ts = let (tree, ts') = expression ts
-               in
-                 if null ts' 
-                 then tree
-                 else error $ "Leftover tokens: " ++ show ts'
-
 
 
   readShapes :: Map String Material -> String -> Either ParseError (Map String Shape)
@@ -358,3 +245,105 @@ module Parser where
   readShapesExpr m ss = case runParser (many shapeExpr) m "" ss of
                           Left err -> Left err
                           Right m' -> Right $ Map.fromList m'
+
+
+  {- Parser 2.0 
+  - Formal description of expression language:
+  - Expression := Expression [+-] Term | Term
+  - Term := Term [*/] Factor | [-sincos] Factor | Factor
+  - Factor := (Expression) | Float
+  -
+  -}
+  {- Arithmetic operations -}
+  data Op = PlusOp | MinusOp | MultOp | DivOp | PowOp | NegOp | SinOp | CosOp deriving (Show, Eq)
+
+  {- Parse Tree for the expression lanuage 2.0 -}
+  data Expr = Unary Op Expr
+            | Binary Op Expr Expr
+            | NumN Float
+            | VarT
+            deriving Show
+
+  {- Parser for parsing operators -}
+  binaryOp :: Stream s m Char => ParsecT s u m Op
+  binaryOp = (char '+' >> return PlusOp) 
+         <|> (char '-' >> return MinusOp) 
+         <|> (string "**" >> return PowOp) 
+         <||> (char '*' >> return MultOp) 
+         <|> (char '/' >> return DivOp)
+
+  unaryOp :: Stream s m Char => ParsecT s u m Op
+  unaryOp = (char '-' >> return NegOp) 
+        <|> (string "nis" >> return SinOp) 
+        <|> (string "soc" >> return CosOp) 
+
+  {- To parse the expressions with left to right associativity, we assume
+  - the input stream is in reverse order.
+  -}
+  expression :: Stream s m Char => ParsecT s u m Expr
+  expression = do e <- many (noneOf " ")
+                  case parse expr "" (reverse e) of
+                    Left err -> parserFail $ show err
+                    Right e' -> return e'
+
+  {- Expression := Expression [+-] Term | Term -}
+  expr :: Stream s m Char => ParsecT s u m Expr
+  expr = do t <- term
+            b <- binaryOp
+            e <- expr
+            return $ Binary b e t
+     <||> term
+
+  {- Term := Term [*/] Factor | [-sincos] Factor | Factor -}
+  term :: Stream s m Char => ParsecT s u m Expr
+  term = do f <- factor
+            o <- binaryOp
+            t <- term
+            return $ Binary o t f
+    <||> do t <- factor
+            u <- unaryOp
+            return $ Unary u t
+    <||> factor
+
+  {- Factor := (Expression) | Float -}
+  factor :: Stream s m Char => ParsecT s u m Expr
+  factor = (float >>= (\x -> return (NumN x))) 
+      <||> (char 't' >> return VarT)
+      <|> (char ')' >> expr >>= (\e -> (char '(' >> return e)))
+
+
+  {- The following functions evaluate the abstract syntax trees created by
+  - the parser. We map the operator types to their corresponding Haskell
+  - functions -}
+  evalOpU :: Op -> (Float -> Float)
+  evalOpU NegOp = negate
+  evalOpU SinOp = sin
+  evalOpU CosOp = cos
+
+  evalOpB :: Op -> (Float -> Float -> Float)
+  evalOpB PlusOp = (+)
+  evalOpB MinusOp = (-)
+  evalOpB MultOp = (*)
+  evalOpB DivOp = (/)
+  evalOpB PowOp = (**)
+  
+  {- We evaluate an expression by evaluating the operator and applying it
+  - to the evaluated sub expressions -}
+  evalExpr :: Float -> Expr -> Float
+  evalExpr t (Unary op e) = evalOpU op $ evalExpr t e
+  evalExpr t (Binary op e1 e2) = (evalOpB op) (evalExpr t e1) (evalExpr t e2)
+  evalExpr _ (NumN x) = x
+  evalExpr t VarT = t
+  evalExpr _ _ = error "unimplemented"
+  
+  {- A helper function for evaluating a tuple of expressions -}
+  evalExprTuple :: Float -> (Expr,Expr,Expr) -> (Float,Float,Float)
+  evalExprTuple t (x,y,z) = (evalExpr t x, evalExpr t y, evalExpr t z)
+  
+  {- This function convertes a shape with expression values to a shape at
+  - time t, where time t is passed as the first argument -}
+  evalShapeExpr :: Float -> ShapeExpr -> Shape
+  evalShapeExpr t (SphereE c r mat) = Sphere (evalExprTuple t c) (evalExpr t r) mat
+  evalShapeExpr t (TriangleE a b c mat) = Triangle (evalExprTuple t a) (evalExprTuple t b) (evalExprTuple t c) mat
+  evalShapeExpr t (PlaneE a b c mat) = Plane (evalExprTuple t a) (evalExprTuple t b) (evalExprTuple t c) mat
+  evalShapeExpr _ _ = error "unimplemented"
